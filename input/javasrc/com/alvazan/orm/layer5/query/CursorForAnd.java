a3f181a23852e8228a405354ac72bdf093856420
Dean Hiller <dean@alvazan.com>
2012-08-31 09:34:29 -0600

    forcing cursor going direct to next cursor instead of bouncing between hasNext and next and hasNext which is VERY VERY confusing so this will help alot with debugging

--- input/javasrc/com/alvazan/orm/layer5/query/CursorForAnd.java
+++ input/javasrc/com/alvazan/orm/layer5/query/CursorForAnd.java
@@ -18,19 +18,34 @@ public class CursorForAnd extends AbstractCursor<IndexColumnInfo> {
 
 	@Override
 	public Holder<IndexColumnInfo> nextImpl() {
-		while(leftResults.hasNext()) {
-			IndexColumnInfo next = leftResults.next();
+		while(true) {
+			Holder<IndexColumnInfo> nextFromCursor = nextFromCursor(leftResults);
+			if(nextFromCursor == null)
+				break;
+			IndexColumnInfo next = nextFromCursor.getValue();
+		
+			//Need to change to lookahead joining here as well as switch join types on the fly
 			//This stinks as we have to re-read from the database every 1000 rows!!!! We should find out if
 			//we can do any kind of counting or something so we don't have to go back to database every time
 			rightResults.beforeFirst();
-			while(rightResults.hasNext()) {
-				IndexColumnInfo andedInfo = rightResults.next();
-				ByteArray key1 = next.getPrimaryKey();
-				ByteArray key2 = andedInfo.getPrimaryKey();
-				if(key1.equals(key2)) {
-					next.setNextAndedColumn(andedInfo);
-					return new Holder<IndexColumnInfo>(next);
-				}
+			Holder<IndexColumnInfo> result = runInnerLoop(next);
+			if(result != null)
+				return result;
+		}
+		return null;
+	}
+
+	private Holder<IndexColumnInfo> runInnerLoop(IndexColumnInfo next) {
+		while(true) {
+			Holder<IndexColumnInfo> nextFromCursor = nextFromCursor(rightResults);
+			if(nextFromCursor == null)
+				break;
+			IndexColumnInfo andedInfo = nextFromCursor.getValue();
+			ByteArray key1 = next.getPrimaryKey();
+			ByteArray key2 = andedInfo.getPrimaryKey();
+			if(key1.equals(key2)) {
+				next.setNextAndedColumn(andedInfo);
+				return new Holder<IndexColumnInfo>(next);
 			}
 		}
 		return null;
