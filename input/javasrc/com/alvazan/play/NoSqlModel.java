acaff9217333a80ff2c0242b61ec4fe9adf00fea
easility <prashant@easility.com>
2012-10-12 15:00:07 +0530

    Changed the build to remove the ClassNotFoundException problem

--- input/javasrc/com/alvazan/play/NoSqlModel.java
+++ input/javasrc/com/alvazan/play/NoSqlModel.java
@@ -15,14 +15,13 @@ import java.util.Set;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import play.data.binding.BeanWrapper;
+
 import play.data.binding.Binder;
 import play.data.binding.ParamNode;
 import play.data.binding.ParamNode.RemovedNode;
 import play.data.validation.Validation;
 import play.exceptions.UnexpectedException;
 
-import com.alvazan.orm.api.base.CursorToMany;
 import com.alvazan.orm.api.base.MetaLayer;
 import com.alvazan.orm.api.base.NoSqlEntityManager;
 import com.alvazan.orm.api.base.anno.NoSqlManyToMany;
