93eab7e7dde372e242f9b5a0f56f93e9f9129dd0
Justin Collins <justin.collins@nrel.gov>
2012-12-11 12:19:40 -0700

    also clear out items from cursor when it is a CursorToMany

--- input/javasrc/com/alvazan/orm/impl/meta/data/MetaCursorField.java
+++ input/javasrc/com/alvazan/orm/impl/meta/data/MetaCursorField.java
@@ -4,6 +4,7 @@ import java.lang.reflect.Field;
 import java.util.List;
 
 import com.alvazan.orm.api.base.CursorToMany;
+import com.alvazan.orm.api.base.CursorToManyImpl;
 import com.alvazan.orm.api.exc.ChildWithNoPkException;
 import com.alvazan.orm.api.z5api.NoSqlSession;
 import com.alvazan.orm.api.z8spi.Row;
@@ -90,6 +91,7 @@ public final class MetaCursorField<OWNER, PROXY> extends MetaAbstractField<OWNER
 			PROXY current = c.getCurrent();
 			translateToColumn(info, current);
 		}
+		((CursorToManyImpl<PROXY>)c).getElementsToAdd().clear();
 	}
 
 	private void addRemoveItems(InfoForIndex<OWNER> info, CursorProxy<PROXY> cursor) {
