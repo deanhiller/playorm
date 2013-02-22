0ce69c8d347d56f74a157f63fde20c7b4b3a9c40
easility <prashant@easility.com>
2013-02-22 13:22:21 +0530

    Add a small script for debugging on windows

---
+++ tools/debug/DebugOnWindows_playorm-cli.bat
@@ -0,0 +1,38 @@
+@echo off
+
+if "%OS%" == "Windows_NT" setlocal
+
+if NOT DEFINED JAVA_HOME goto :err
+
+REM Ensure that any user defined CLASSPATH variables are not used on startup
+set CLASSPATH=
+
+REM For each jar in the lib directory call append to build the CLASSPATH variable.
+for %%i in ("..\output\jardist\*.jar") do call :append "%%i"
+
+goto okClasspath
+
+:append
+set CLASSPATH=%CLASSPATH%;%1
+goto :eof
+
+:okClasspath
+REM Include the \classes directory so it works in development
+set PLAYORM_CLASSPATH="..\bin";%CLASSPATH%;
+
+SET A_PORT=8000
+SET A_DBG=-Xdebug -Xrunjdwp:transport=dt_socket,address=127.0.0.1:8000,suspend=y
+goto runCli
+
+
+:runCli
+"%JAVA_HOME%\bin\java" %A_DBG% -cp %PLAYORM_CLASSPATH% com.alvazan.ssql.cmdline.PlayOrm %*
+goto finally
+
+:err
+echo The JAVA_HOME environment variable must be set to run this program!
+pause
+
+:finally
+
+ENDLOCAL
