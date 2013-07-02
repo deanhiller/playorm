2afaa67ff2b9d65d73136884bde6c9d0f14cd25b
Dean Hiller <dean@alvazan.com>
2013-07-02 15:21:07 -0600

    add a callback so clients can more define the column family definition during creation time

---
+++ src/main/java/com/alvazan/orm/api/z8spi/CreateCfCallback.java
@@ -0,0 +1,8 @@
+package com.alvazan.orm.api.z8spi;
+
+
+public interface CreateCfCallback {
+
+	public Object modifyColumnFamily(String cfName, Object def);
+
+}
--- src/main/java/com/alvazan/orm/api/z8spi/SpiConstants.java
+++ src/main/java/com/alvazan/orm/api/z8spi/SpiConstants.java
@@ -3,5 +3,6 @@ package com.alvazan.orm.api.z8spi;
 public class SpiConstants {
 
 	public static final String CASSANDRA_BUILDER = "nosql.cassandra.builder";
+	public static final String CASSANDRA_CF_CREATE_CALLBACK = "nosql.cassandra.createcfcallback";
 	
 }
