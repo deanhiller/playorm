2779249f4c5c6a491d21c80494437966c4886450
Dean Hiller <dean@alvazan.com>
2013-07-03 14:31:03 -0600

    fix the checkstyle issue

--- src/test/java/com/alvazan/test/TestInheritanceNestedSingleTable.java
+++ src/test/java/com/alvazan/test/TestInheritanceNestedSingleTable.java
@@ -10,7 +10,8 @@ import com.alvazan.test.db.InheritedNestedC;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
-import org.junit.Test;
+//broke checkstyle so remove...
+//import org.junit.Test;
 
 import java.util.Arrays;
 import java.util.List;
