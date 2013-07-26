af517e01483dd2b6127ef66f64ef9fffbad337b3
Dean Hiller <dean@alvazan.com>
2013-07-26 10:48:29 -0600

    add a callback for creation of keyspace so clients can customize that

--- src/main/java/com/alvazan/orm/api/z8spi/CreateCfCallback.java
+++ src/main/java/com/alvazan/orm/api/z8spi/CreateCfCallback.java
@@ -1,8 +1,22 @@
 package com.alvazan.orm.api.z8spi;
 
 
+
 public interface CreateCfCallback {
 
+	/** 
+	 * For cassandra, def param is hte ColumnFamilyDefinition and you return that
+	 * @param cfName
+	 * @param def
+	 * @return
+	 */
 	public Object modifyColumnFamily(String cfName, Object def);
+	/** 
+	 * For cassandra, def param is the KeyspaceDefinition and you return that
+	 * @param cfName
+	 * @param def
+	 * @return
+	 */
+	public Object configureKeySpace(String keyspaceName, Object def);
 
 }
